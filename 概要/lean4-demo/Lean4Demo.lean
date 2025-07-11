5de3f09f4ae84826b2cd746ac0228a760d43ba73
ych817 <ych817@aucklanduni.ac.nz>
2025-07-11 20:48:30 +1200

    更新米田引理

--- 概要/lean4-demo/Lean4Demo.lean
+++
@@ -1,3 +0,0 @@
--- This module serves as the root of the `Lean4Demo` library.
--- Import modules here that should be built as part of the library.
-import Lean4Demo.Basic
--- 概要/lean4-demo/Main.lean
+++
@@ -1,4 +0,0 @@
-import Lean4Demo
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
